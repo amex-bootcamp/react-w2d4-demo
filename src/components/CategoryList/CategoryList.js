53b210d81fbb9e2e7890ca9b08dfd2139b6decd2
kchia <kchia87@gmail.com>
2021-06-17 16:03:18 -0400

    update the CategoryList

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
 
 export default class CategoryList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://jservice.io/api/categories?count=10")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("data", data);
+        this.setState({
+          categories: data,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+
   render() {
-    return <div>CategoryList</div>;
+    const categoryListItems = this.state.categories.map((category, index) => {
+      return <li>{`${category.title}, ${category.id}`}</li>;
+    });
+    return (
+      <div>
+        <h2>List of Top Categories</h2>
+        <ol>{categoryListItems}</ol>
+      </div>
+    );
   }
 }
