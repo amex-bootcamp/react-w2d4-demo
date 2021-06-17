d9725480a13efa855f7f512e8a2471ce7512ef8e
kchia <kchia87@gmail.com>
2021-06-17 16:48:12 -0400

    create the QuestionByCategoryList component

--- src/components/QuestionByCategoryList/QuestionByCategoryList.js
+++ src/components/QuestionByCategoryList/QuestionByCategoryList.js
@@ -1,7 +1,69 @@
-import React, { Component } from "react";
+import { Component } from "react";
 
-export default class QuestionByCategoryList extends Component {
+class QuestionByCategoryList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      heading: "Get Random Trivia Questions By Category",
+      categoryId: undefined,
+      questions: [],
+    };
+  }
+
+  handleChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { categoryId } = this.state;
+    fetch(`http://jservice.io/api/category?id=${categoryId}`)
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          heading: `${data.title} Trivia Questions`,
+          questions: data.clues,
+        })
+      )
+      .catch(console.log);
+  };
+
   render() {
-    return <div>QuestionByCategoryList</div>;
+    const { heading, categoryId, questions } = this.state;
+
+    const questionListItems = questions.map((clue, index) => {
+      const { question } = clue;
+
+      return (
+        <li key={`${question}-${index}`}>{`Question ${
+          index + 1
+        }: ${question}`}</li>
+      );
+    });
+
+    return (
+      <section>
+        <h2>{heading}</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="categoryId">
+            Enter category ID:
+            <input
+              type="text"
+              id="categoryId"
+              name="categoryId"
+              onChange={this.handleChange}
+              value={categoryId}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <ul>{questionListItems}</ul>
+      </section>
+    );
   }
 }
+
+export default QuestionByCategoryList;
