2b2ed7102b2a2f336f604b52a820d33a792f2a20
kchia <kchia87@gmail.com>
2021-06-17 14:28:58 -0400

    add state to the Question component

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
 
 export default class Question extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      question: "ad",
+      answer: "asdf",
+      category: "asdf",
+    };
+  }
+
   render() {
-    return <div>Question</div>;
+    return (
+      <div>
+        <h2>Question: {this.state.question}</h2>
+        <h3>Answer: {this.state.answer}</h3>
+        <p>Category: {this.state.category}</p>
+      </div>
+    );
   }
 }
