effe018fd12865676c61409862e880d463031ceb
kchia <kchia87@gmail.com>
2021-06-17 14:43:31 -0400

    don't put this.setState within the render method

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -9,9 +9,32 @@ export default class Question extends Component {
       answer: "asdf",
       category: "asdf",
     };
+
+    console.log("this is the constructor method");
+  }
+
+  // componentDidMount gets called after every re-render of the page
+  componentDidMount() {
+    console.log("this is the componentDidMount method");
   }
 
   render() {
+    console.log("this is the render method");
+
+    // Putting this.setState inside render() will cause infinite re-renders
+    fetch("http://jservice.io/api/random")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("data from API", data);
+        // No more DOM manipulation!
+        // call this.setState here to update the DOM
+        this.setState({
+          question: data[0].question,
+          answer: data[0].answer,
+          category: data[0].category.title,
+        });
+      });
+
     return (
       <div>
         <h2>Question: {this.state.question}</h2>
