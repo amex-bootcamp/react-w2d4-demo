03cdbb2d99b17886ba77ea0b08e36339528a2e59
kchia <kchia87@gmail.com>
2021-06-18 12:41:10 -0400

    react router: UserList loads users data on component load

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,7 +1,36 @@
 import React, { Component } from "react";
 
 export default class UserList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Data", data);
+
+        this.setState({
+          users: data,
+        });
+      });
+  }
+
   render() {
-    return <h1>This is my UserList page</h1>;
+    const userListItems = this.state.users.map((user, index) => {
+      return <li key={`${user.name}-${index}`}>{user.name}</li>;
+    });
+
+    return (
+      <section>
+        <h2>Welcome to the directory of users:</h2>
+        <ul>{userListItems}</ul>
+      </section>
+    );
   }
 }
