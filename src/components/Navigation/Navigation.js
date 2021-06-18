1acb953cfaf57d5d83453b639fa57427d00a6bf8
kchia <kchia87@gmail.com>
2021-06-18 10:59:15 -0400

    react router: turn Home link into a react router link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 
+import { Link } from "react-router-dom";
+
 export default class Navigation extends Component {
   render() {
     return (
       <nav>
         <ul>
-          <li>Home</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
           <li>About</li>
           <li>Users</li>
           <li>Posts</li>
