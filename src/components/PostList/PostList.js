56bf437bc4e325e88164759a6260f4568cb097d1
kchia <kchia87@gmail.com>
2021-06-18 14:58:49 -0400

    react router: build out the PostList component

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
 
 export default class PostList extends Component {
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     posts: ["post 1", "post 2", "post 3", "post 4"],
+  //   };
+  // }
+
+  state = {
+    posts: ["post 1", "post 2", "post 3", "post 4"],
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => this.setState({ posts: data }))
+      .catch((error) => console.log(error));
+  }
+
   render() {
-    return <h1>This is my PostList page</h1>;
+    return (
+      <section>
+        <h2>Here are a list of posts:</h2>
+        <ul>
+          {this.state.posts.map((post, index) => {
+            return <li key={`${post.title}-${index}`}>{post.title}</li>;
+          })}
+        </ul>
+      </section>
+    );
   }
 }
